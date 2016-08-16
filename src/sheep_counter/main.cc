2c9b09a5b5cee3c01ede26634948d41c9035464a
DenisHuberTUM <huber.denis@mytum.de>
2016-08-16 17:26:38 +0200

    Testing thread state of local thread

--- src/sheep_counter/main.cc
+++ src/sheep_counter/main.cc
@@ -12,9 +12,35 @@
 
 #include <base/component.h>
 #include <base/log.h>
+#include <base/thread.h>
+#include <base/sleep.h>
 #include <timer_session/connection.h>
+#include <cpu_thread/client.h>
 
-Genode::size_t Component::stack_size() { return 32*4*1024; }
+class My_thread : public Genode::Thread
+{
+private:
+	Genode::Env &_env;
+public:
+	My_thread(Genode::Env &env, const char *name, Genode::Cpu_session &cpu)
+	:
+		Thread(env, name, 0x2000, Thread::Location(), Thread::Weight(), cpu), _env(env)
+    { }
+
+	void entry()
+	{
+		int i = 0;
+		Genode::log("Hello from thread ", name().string());
+		while(1)
+		{
+			if(i >= 10) i--;
+			else i++;
+		}
+
+	}
+};
+
+Genode::size_t Component::stack_size() { return 16*1024; }
 
 void Component::construct(Genode::Env &env)
 {
@@ -33,10 +59,25 @@ void Component::construct(Genode::Env &env)
         break;
     }
     */
-
+    /*
     Ram_dataspace_capability ram_ds = env.ram().alloc(4096);
     char *local_addr = env.rm().attach(ram_ds);
     *local_addr = 'c';
     env.rm().detach(local_addr);
+    */
+    const char thread0_name[] = "Worker";
+    My_thread thread0 { env, thread0_name, env.cpu() };
+    thread0.start();
+
+    timer.msleep(2000);
+
+    Genode::Thread_state state { Genode::Cpu_thread_client(thread0.cap()).state() };
+    Genode::log("Worker1: ip ", state.ip, ", sp ", state.sp);
+
+    const char thread1_name[] = "Worker2";
+    My_thread thread1 { env, thread1_name, env.cpu() };
+    thread1.start();
+    thread1.join();
+
     log("3 sheeps. zzZ");
 }
