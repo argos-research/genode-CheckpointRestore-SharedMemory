2312a2654b420a5ed51e0e39edd562158e779556
DenisHuberTUM <huber.denis@mytum.de>
2016-08-25 14:14:32 +0200

    Creating a scenario where the rm session runs out of memory, and the exception can not be caught, because it happens on another thread (probably on the core entrypoint for services)

--- src/test/stress_rm_creation/main.cc
+++ src/test/stress_rm_creation/main.cc
@@ -1,5 +1,5 @@
 /*
- * \brief
+ * \brief  Creates managed dataspaces with ram dataspaces
  * \author Denis Huber
  * \date   2016-08-20
  */
@@ -8,6 +8,8 @@
 #include <base/env.h>
 #include <base/log.h>
 #include <base/component.h>
+#include <rm_session/connection.h>
+#include <region_map/client.h>
 
 using namespace Genode;
 
@@ -15,5 +17,23 @@ size_t Component::stack_size() { return 64*1024; }
 
 void Component::construct(Genode::Env &env)
 {
-	log("Hello from Rm-creation-stresser");
+	log("--- Rm-creation-stresser started ---");
+	Rm_connection rm_con{env};
+
+	//env.parent().upgrade(rm_con.cap(), "ram_quota=131072");
+
+	for(unsigned int i = 0; i < 10; i++)
+	{
+		log("Round ", i);
+		try
+		{
+			rm_con.create(4096);
+		}
+		catch(Allocator::Out_of_memory)
+		{
+			log("Exception caught!");
+		}
+	}
+
+	log("--- Rm-creation-stresser ended ---");
 }
