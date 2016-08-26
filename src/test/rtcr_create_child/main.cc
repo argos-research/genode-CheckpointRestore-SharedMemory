ee3f8295880025a8638fe5c9d3434e6aca0f4bf2
DenisHuberTUM <huber.denis@mytum.de>
2016-08-26 13:12:37 +0200

    Adding test for creating a child whose PD, CPU and RAM sessions are intercepted by the parent.

---
+++ src/test/rtcr_create_child/main.cc
@@ -0,0 +1,40 @@
+/*
+ * \brief  Rtcr child creation
+ * \author Denis Huber
+ * \date   2016-08-26
+ */
+
+/* Genode include */
+#include <base/component.h>
+#include <base/signal.h>
+#include <base/sleep.h>
+#include <base/log.h>
+
+/* Rtcr includes */
+#include "../../rtcr/target_child.h"
+
+namespace Rtcr {
+	struct Main;
+}
+
+struct Rtcr::Main
+{
+	enum { ROOT_STACK_SIZE = 16*1024 };
+	Genode::Env             &env;
+	Genode::Heap             md_heap         { env.ram(), env.rm() };
+	Genode::Service_registry parent_services { };
+
+	Main(Genode::Env &env_) : env(env_)
+	{
+		Target_child child { env, md_heap, parent_services, "sheep_counter" };
+
+		Genode::sleep_forever();
+	}
+};
+
+Genode::size_t Component::stack_size() { return 64*1024; }
+
+void Component::construct(Genode::Env &env)
+{
+	static Rtcr::Main main(env);
+}
---
+++ src/test/rtcr_create_child/target.mk
@@ -0,0 +1,3 @@
+TARGET = rtcr_child_creator
+SRC_CC = main.cc
+LIBS   = base
