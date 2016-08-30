a646f3a9583442e750eba88baf2b4ee0d3c79ffb
DenisHuberTUM <huber.denis@mytum.de>
2016-08-30 13:58:12 +0200

    Adding log output.

--- src/test/sheep_counter/main.cc
+++ src/test/sheep_counter/main.cc
@@ -20,8 +20,10 @@ Genode::size_t Component::stack_size() { return 16*1024; }
 void Component::construct(Genode::Env &env)
 {
 	using namespace Genode;
+	log("Creating Timer session.");
 	Timer::Connection timer(env);
 
+	log("Allocating and attaching memory and its dataspace.");
 	unsigned int *addr = env.rm().attach(env.ram().alloc(4096));
 	addr[0] = 1;
 	unsigned int &n = *addr;
@@ -35,5 +37,6 @@ void Component::construct(Genode::Env &env)
 		n++;
 		timer.msleep(1000);
 	}
+
 
 }
