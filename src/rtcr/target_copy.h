26e19a624a012d5855dc38759de8713307a67b43
DenisHuberTUM <huber.denis@mytum.de>
2016-09-07 19:19:00 +0200

    Extending Target_copy by member attributes

--- src/rtcr/target_copy.h
+++ src/rtcr/target_copy.h
@@ -1,5 +1,5 @@
 /*
- * \brief  Child creation
+ * \brief  Child copy
  * \author Denis Huber
  * \date   2016-09-07
  */
@@ -16,13 +16,30 @@ namespace Rtcr {
 class Rtcr::Target_copy : Genode::List<Target_copy>::Element
 {
 private:
+	Genode::List<Rtcr::Thread_info>          &_threads;
+	Genode::List<Rtcr::Attached_region_info> &_attached_regions;
+	Genode::List<Rtcr::Managed_region_info>  &_managed_regions;
+	Genode::Allocator                        &_alloc;
 
+	Genode::Lock                             _copy_lock;
+	Genode::List<Rtcr::Thread_info>          _copied_threads;
+	Genode::List<Rtcr::Attached_region_info> _copied_regions;
 
 public:
-	Target_copy(Genode::List<Rtcr::Thread_info> &threads, Genode::List<Rtcr::Attached_region_info> &attached_regions)
-	{
+	Target_copy(Genode::List<Rtcr::Thread_info>      &threads,
+			Genode::List<Rtcr::Attached_region_info> &attached_regions,
+			Genode::List<Rtcr::Managed_region_info>  &managed_regions,
+			Genode::Allocator                        &alloc)
+	:
+		_threads         (threads),
+		_attached_regions(attached_regions),
+		_managed_regions (managed_regions),
+		_alloc           (alloc),
+		_copy_lock       (),
+		_copied_threads  (),
+		_copied_regions  ()
+	{ }
 
-	}
 
 };
 
