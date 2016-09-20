fa6494a6c155bce6bc529c5e0e3517343abef73a
DenisHuberTUM <huber.denis@mytum.de>
2016-09-20 17:41:49 +0200

    Adding base functionality to request and install capabilities

--- src/test/concept_capabilities/client/main.cc
+++ src/test/concept_capabilities/client/main.cc
@@ -1,13 +1,18 @@
 /*
- * \brief  Template test
+ * \brief  Client which cap space will be manipulated
  * \author Denis Huber
- * \date   2016-08-20
+ * \date   2016-09-20
  */
 
 /* Genode includes */
 #include <base/env.h>
 #include <base/log.h>
+#include <base/sleep.h>
 #include <base/component.h>
+#include <timer_session/connection.h>
+
+/* Resource includes */
+#include <resource_session/connection.h>
 
 using namespace Genode;
 
@@ -15,5 +20,14 @@ size_t Component::stack_size() { return 64*1024; }
 
 void Component::construct(Genode::Env &env)
 {
-	log("Hello world!");
+	log("Initialization started");
+	Timer::Connection timer {env};
+
+	log("Requesting session to Resource service");
+	Resource::Connection resource_service {env};
+
+	log("Sending native pd cap");
+	resource_service.provide(env.pd().native_pd());
+
+	sleep_forever();
 }
--- src/test/concept_capabilities/server/main.cc
+++ src/test/concept_capabilities/server/main.cc
@@ -1,31 +1,39 @@
 /*
- * \brief  Template test
+ * \brief  Server which manipulates the cap space of the client
  * \author Denis Huber
- * \date   2016-08-20
+ * \date   2016-09-20
  */
 
 /* Genode includes */
 #include <base/env.h>
 #include <base/log.h>
+#include <base/sleep.h>
 #include <base/component.h>
-#include <root/component.h>
 #include <base/rpc_server.h>
+#include <root/component.h>
 #include <timer_session/connection.h>
+#include <pd_session/pd_session.h>
+
+/* Fiasco.OC includes */
+#include <foc_native_pd/client.h>
+
+/* Resource includes */
 #include <resource_session/resource_session.h>
 
 namespace Resource {
 	struct Client_resources;
-	class Session_component;
-	class Root;
+	class  Session_component;
+	class  Root;
+	struct Main;
 }
 
 struct Resource::Client_resources
 {
-	Genode::Pd_session_capability pd_cap;
+	Genode::Capability<Genode::Pd_session::Native_pd> native_pd_cap;
 
 	Client_resources()
 	:
-		pd_cap()
+		native_pd_cap()
 	{}
 };
 
@@ -40,9 +48,9 @@ public:
 		_cli_res(cli_res)
 	{ }
 
-	void provide(Genode::Native_capability pd_cap, Genode::uint32_t)
+	void provide(Genode::Native_capability native_pd_cap, Genode::uint32_t)
 	{
-		_cli_res.pd_cap = Genode::reinterpret_cap_cast<Genode::Pd_session>(pd_cap);
+		_cli_res.native_pd_cap = Genode::reinterpret_cap_cast<Genode::Pd_session::Native_pd>(native_pd_cap);
 	}
 	Genode::Native_capability request(Genode::uint32_t)
 	{
@@ -67,26 +75,51 @@ public:
 	{ }
 };
 
-struct Main
+struct Resource::Main
 {
 	Genode::Env                &env;
 	Resource::Client_resources  cli_res;
 	Genode::Sliced_heap         sliced_heap;
 	Genode::Entrypoint          session_ep;
 	Resource::Root              root;
+	Timer::Connection           timer;
+
 
 	Main(Genode::Env &env)
 	:
-		env(env),
-		cli_res(),
-		sliced_heap(env.ram(), env.rm()),
-		session_ep(env, 16*1024, "session ep"),
-		root(session_ep, sliced_heap, cli_res)
+		env         (env),
+		cli_res     (),
+		sliced_heap (env.ram(), env.rm()),
+		session_ep  (env, 16*1024, "session ep"),
+		root        (session_ep, sliced_heap, cli_res),
+		timer       (env)
 	{
 		using Genode::log;
 
 		log("Announcing Resource service");
 		env.parent().announce(session_ep.manage(root));
+
+		while(true)
+		{
+			if(cli_res.native_pd_cap.valid())
+			{
+				log("Valid pd_cap");
+				Genode::Capability<Genode::Foc_native_pd> foc_cap =
+						Genode::static_cap_cast<Genode::Foc_native_pd>(cli_res.native_pd_cap);
+				//log("foc_pd_cap is ", foc_cap.valid()?"valid":"invalid");
+				Genode::Foc_native_pd_client foc_client{foc_cap};
+				foc_client.request(1);
+				foc_client.install(foc_cap, 1);
+			}
+			else
+			{
+				log("Invalid pd_cap");
+			}
+
+			timer.msleep(4000);
+		}
+
+		Genode::sleep_forever();
 	}
 };
 
@@ -94,5 +127,5 @@ Genode::size_t Component::stack_size() { return 64*1024; }
 
 void Component::construct(Genode::Env &env)
 {
-	static Main main(env);
+	static Resource::Main main(env);
 }
