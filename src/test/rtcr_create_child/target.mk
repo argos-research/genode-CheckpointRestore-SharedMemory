94bcd1af33e3ce4a3468d3d86c964ca388d9ceb5
DenisHuberTUM <huber.denis@mytum.de>
2016-10-06 15:36:17 +0200

    Adjusting to header/impl separation

--- src/test/rtcr_create_child/target.mk
+++ src/test/rtcr_create_child/target.mk
@@ -1,3 +1,20 @@
 TARGET = rtcr_child_creator
-SRC_CC = main.cc
-LIBS   = base
+
+SRC_CC += main.cc \
+          cpu_session_component.cc \
+          pd_session_component.cc \
+          ram_session_component.cc \
+          region_map_component.cc \
+          log_session.cc \
+          rm_session.cc \
+          timer_session.cc
+
+LIBS   += base
+
+vpath cpu_session_component.cc $(REP_DIR)/src/rtcr/intercept
+vpath pd_session_component.cc  $(REP_DIR)/src/rtcr/intercept
+vpath ram_session_component.cc $(REP_DIR)/src/rtcr/intercept
+vpath region_map_component.cc  $(REP_DIR)/src/rtcr/intercept
+vpath log_session.cc           $(REP_DIR)/src/rtcr/intercept
+vpath rm_session.cc            $(REP_DIR)/src/rtcr/intercept
+vpath timer_session.cc         $(REP_DIR)/src/rtcr/intercept
