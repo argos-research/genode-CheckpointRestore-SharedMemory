3e2cac1b098171bbb7657a4d0a2d7aa4d5b787a9
DenisHuberTUM <huber.denis@mytum.de>
2016-10-24 15:39:42 +0200

    Adding new test for restoration

---
+++ src/test/rtcr_restore_child/main.cc
@@ -0,0 +1,57 @@
+/*
+ * \brief  Rtcr child creation
+ * \author Denis Huber
+ * \date   2016-08-26
+ */
+
+/* Genode include */
+#include <base/component.h>
+#include <base/signal.h>
+#include <base/sleep.h>
+#include <base/log.h>
+#include <timer_session/connection.h>
+
+/* Rtcr includes */
+#include "../../rtcr/target_child.h"
+#include "../../rtcr/target_copy.h"
+
+namespace Rtcr {
+	struct Main;
+}
+
+struct Rtcr::Main
+{
+	enum { ROOT_STACK_SIZE = 16*1024 };
+	Genode::Env             &env;
+	Genode::Heap             md_heap         { env.ram(), env.rm() };
+	Genode::Service_registry parent_services { };
+
+	Main(Genode::Env &env_) : env(env_)
+	{
+		using namespace Genode;
+
+		Timer::Connection timer { env };
+
+		Target_child child { env, md_heap, parent_services, "sheep_counter", 0 };
+		child.start();
+
+		timer.msleep(3000);
+
+		child.pause();
+		Target_copy copy { env, md_heap, child };
+		copy.checkpoint();
+
+
+		Target_child child2 { env, md_heap, parent_services, "sheep_counter", 0 };
+		child2.start(copy);
+
+		Genode::sleep_forever();
+	}
+};
+
+Genode::size_t Component::stack_size() { return 32*1024; }
+
+void Component::construct(Genode::Env &env)
+{
+	static Rtcr::Main main(env);
+}
