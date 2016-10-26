61b1bea20f2cce69e6c2bd70ebe8f07a2d741f19
DenisHuberTUM <huber.denis@mytum.de>
2016-10-26 12:34:28 +0200

    Implementing copy ctor with helper methods

--- src/rtcr/target_state.cc
+++ src/rtcr/target_state.cc
@@ -10,7 +10,7 @@ using namespace Rtcr;
 
 
 template <typename T>
-void Target_state::_delete_list(Genode::List<T>& infos)
+void Target_state::_delete_list(Genode::List<T> &infos)
 {
 	while(T* info = infos.first())
 	{
@@ -19,17 +19,22 @@ void Target_state::_delete_list(Genode::List<T>& infos)
 	}
 }
 // Template instanciation
-template void Target_state::_delete_list(Genode::List<Stored_thread_info>& infos);
+template void Target_state::_delete_list(Genode::List<Stored_thread_info> &infos);
 
 
-void Target_state::_delete_list(Genode::List<Stored_region_info>& infos)
+void Target_state::_delete_list(Genode::List<Stored_dataspace_info> &infos)
 {
-	// Free RAM dataspaces and delete list elements
+	while(Stored_dataspace_info* info = infos.first())
+	{
+		infos.remove(info);
+		_env.ram().free(Genode::static_cap_cast<Genode::Ram_dataspace>(info->ds_cap));
+		Genode::destroy(_alloc, info);
+	}
 }
 
 
 template <typename T>
-void Target_state::_copy_list(Genode::List<T>& from_infos, Genode::List<T>& to_infos)
+void Target_state::_copy_list(Genode::List<T> &from_infos, Genode::List<T> &to_infos)
 {
 	// Exit, if the list is not empty
 	if(to_infos.first()) return;
@@ -37,6 +42,7 @@ void Target_state::_copy_list(Genode::List<T>& from_infos, Genode::List<T>& to_i
 	T *from_info = from_infos.first();
 	while(from_info)
 	{
+		// Use copy ctor of T
 		T *to_info = new (_alloc) T(*from_info);
 		to_infos.insert(to_info);
 
@@ -44,23 +50,71 @@ void Target_state::_copy_list(Genode::List<T>& from_infos, Genode::List<T>& to_i
 	}
 }
 // Template instanciation
-template void Target_state::_copy_list(Genode::List<Stored_thread_info>& from_infos, Genode::List<Stored_thread_info>& to_infos);
+template void Target_state::_copy_list(Genode::List<Stored_thread_info> &from_infos, Genode::List<Stored_thread_info> &to_infos);
 
 
-Target_state::Target_state(Genode::Allocator& alloc)
+void Target_state::_copy_list(Genode::List<Stored_dataspace_info> &from_infos, Genode::List<Stored_dataspace_info> &to_infos)
+{
+	// Exit, if the list is not empty
+	if(to_infos.first()) return;
+
+	Stored_dataspace_info *from_info = from_infos.first();
+	while(from_info)
+	{
+		// Create new dataspace and copy the contents of from_info to it
+		Genode::Ram_dataspace_capability new_ds_cap = _env.ram().alloc(from_info->size);
+		_copy_dataspace(from_info->ds_cap, new_ds_cap, from_info->size);
+
+		// Use copy ctor
+		Stored_dataspace_info *to_info = new (_alloc) Stored_dataspace_info(*from_info);
+		to_info->ds_cap = new_ds_cap;
+
+		to_infos.insert(to_info);
+
+		from_info = from_info->next();
+	}
+}
+
+
+void Target_state::_copy_dataspace(Genode::Dataspace_capability source_ds_cap, Genode::Dataspace_capability dest_ds_cap,
+		Genode::size_t size, Genode::off_t dest_offset)
+{
+	char *source = _env.rm().attach(source_ds_cap);
+	char *dest   = _env.rm().attach(dest_ds_cap);
+
+	Genode::memcpy(dest + dest_offset, source, size);
+
+	_env.rm().detach(dest);
+	_env.rm().detach(source);
+}
+
+
+Target_state::Target_state(Genode::Env &env, Genode::Allocator &alloc)
 :
-	_alloc(alloc)
+	_env   (env),
+	_alloc (alloc)
 { }
 
 
-Target_state::Target_state(Target_state& other)
+Target_state::Target_state(Target_state &other)
 :
-	_alloc(other._alloc)
-{ }
+	_env   (other._env),
+	_alloc (other._alloc)
+{
+	_copy_list(other._stored_threads,       _stored_threads);
+	_copy_list(other._stored_address_space, _stored_address_space);
+	_copy_list(other._stored_stack_area,    _stored_stack_area);
+	_copy_list(other._stored_linker_area,   _stored_linker_area);
+	_copy_list(other._stored_dataspaces,    _stored_dataspaces);
+}
 
 
 Target_state::~Target_state()
 {
 	_delete_list(_stored_threads);
 	_delete_list(_stored_address_space);
+	_delete_list(_stored_stack_area);
+	_delete_list(_stored_linker_area);
+	_delete_list(_stored_dataspaces);
 }
+
--- src/rtcr/target_state.h
+++ src/rtcr/target_state.h
@@ -10,11 +10,12 @@
 /* Genode includes */
 #include <util/list.h>
 #include <base/heap.h>
+#include <base/env.h>
 
 /* Rtcr includes */
+#include "child_state/stored_dataspace_info.h"
 #include "child_state/stored_region_info.h"
 #include "child_state/stored_thread_info.h"
-#include "child_state/stored_ram_ds_info.h"
 
 namespace Rtcr {
 	class  Target_state;
@@ -23,26 +24,28 @@ namespace Rtcr {
 class Rtcr::Target_state
 {
 private:
+	Genode::Env        &_env;
 	Genode::Allocator &_alloc;
 
-	Genode::List<Stored_thread_info> _stored_threads;
-	Genode::List<Stored_region_info> _stored_address_space;
-	Genode::List<Stored_region_info> _stored_stack_area;
-	Genode::List<Stored_region_info> _stored_linker_area;
+	Genode::List<Stored_thread_info>    _stored_threads;
+	Genode::List<Stored_region_info>    _stored_address_space;
+	Genode::List<Stored_region_info>    _stored_stack_area;
+	Genode::List<Stored_region_info>    _stored_linker_area;
+	Genode::List<Stored_dataspace_info> _stored_dataspaces;
 
 	/**
-	 * Deletes all members of the list
+	 * Deletes all list elements
 	 *
 	 * It is used primarily by the destructor
 	 */
 	template <typename T>
 	void _delete_list(Genode::List<T> &infos);
 	/**
-	 * Specialized method to delete Stored_region_infos from list and free the corresponding RAM dataspaces
+	 * Specialized method to delete Stored_dataspace_infos from list and free the corresponding dataspaces
 	 *
 	 * It is used primarily by the destructor
 	 */
-	void _delete_list(Genode::List<Stored_region_info> &infos);
+	void _delete_list(Genode::List<Stored_dataspace_info> &infos);
 	/**
 	 * Copy all members of the list from_infos to to_infos
 	 *
@@ -50,11 +53,25 @@ private:
 	 */
 	template <typename T>
 	void _copy_list(Genode::List<T> &from_infos, Genode::List<T> &to_infos);
+	/**
+	 * Specialized method to copy all list elements of the list from_infos to to_infos and allocate new dataspaces
+	 * to store the dataspace content
+	 *
+	 * It is used primarily by copy constructor
+	 */
+	void _copy_list(Genode::List<Stored_dataspace_info> &from_infos, Genode::List<Stored_dataspace_info> &to_infos);
+	/**
+	 * Copy dataspace's content
+	 */
+	void _copy_dataspace(Genode::Dataspace_capability source_ds_cap, Genode::Dataspace_capability dest_ds_cap,
+			Genode::size_t size, Genode::off_t dest_offset = 0);
 
 public:
-	Target_state(Genode::Allocator &alloc);
+	Target_state(Genode::Env &env, Genode::Allocator &alloc);
 	Target_state(Target_state &other);
 	~Target_state();
+
+	Target_state& operator=(const Target_state &other) = delete;
 };
 
 #endif /* _RTCR_TARGET_STATE_H_ */
