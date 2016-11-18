a72a2d8fdf65d49a53171749b3c1a62f64aa62ee
DenisHuberTUM <huber.denis@mytum.de>
2016-11-18 18:09:15 +0100

    Implementing restore methods for signal context and signal source

--- src/rtcr/restorer.cc
+++ src/rtcr/restorer.cc
@@ -8,40 +8,131 @@
 
 using namespace Rtcr;
 
-Restorer::Restorer(Target_child &child, Target_state &state)
-: _child(child), _state(state) { }
+
+void Restorer::_restore_pd_session(Pd_session_component &child_obj, Stored_pd_session_info &state_info)
+{
+	_restore_sources(child_obj, state_info.stored_source_infos);
+	_restore_contexts(child_obj, state_info.stored_context_infos);
+	_restore_region_map(child_obj.address_space_component(), state_info.stored_address_space);
+	_restore_region_map(child_obj.stack_area_component(), state_info.stored_stack_area);
+	_restore_region_map(child_obj.linker_area_component(), state_info.stored_linker_area);
+}
+
+
+void Restorer::_restore_sources(Pd_session_component &child_obj, Genode::List<Stored_signal_source_info> &state_infos)
+{
+	Stored_signal_source_info *state_info = nullptr;
+	while(state_info)
+	{
+		// Create RPC object
+		Genode::Capability<Genode::Signal_source> source_cap = child_obj.alloc_signal_source();
+
+		// Create translation of ckpt_badge to resto_badge
+		Badge_badge_info *info = new (_alloc) Badge_badge_info(state_info->badge, source_cap.local_name());
+		_badge_dictionary.insert(info);
+
+		state_info = state_info->next();
+	}
+}
+
+
+void Restorer::_restore_contexts(Pd_session_component &child_obj, Genode::List<Stored_signal_context_info> &state_infos)
+{
+	Stored_signal_context_info *state_info = nullptr;
+	while(state_info)
+	{
+		// Find source_cap in PD session
+		Genode::Capability<Genode::Signal_source> source_cap;
+		if(state_info->signal_source_badge != 0)
+		{
+			// Find badge of restored RPC object
+			Badge_badge_info *badge_info = _badge_dictionary.first();
+			if(badge_info) badge_info = badge_info->find_by_ckpt_badge(state_info->badge);
+			if(!badge_info)
+			{
+				Genode::error("No resto badge for ckpt badge ", state_info->badge," of signal source found!");
+				throw Genode::Exception();
+			}
+
+			// Find restored RPC object
+			Signal_source_info *ss_info = child_obj.signal_source_infos().first();
+			if(ss_info) ss_info = ss_info->find_by_badge(badge_info->resto_badge);
+			if(!ss_info)
+			{
+				Genode::error("No signal source for resto badge ", badge_info->resto_badge, " found!");
+				throw Genode::Exception();
+			}
+
+			source_cap = ss_info->cap;
+		}
+
+		// Create RPC object
+		Genode::Capability<Genode::Signal_context> context_cap = child_obj.alloc_context(source_cap, state_info->imprint);
+
+		// Create translation of ckpt_badge to resto_badge
+		Badge_badge_info *info = new (_alloc) Badge_badge_info(state_info->badge, context_cap.local_name());
+		_badge_dictionary.insert(info);
+
+		state_info = state_info->next();
+	}
+}
+
+
+void Restorer::_restore_region_map(Region_map_component &child_obj, Stored_region_map_info &state_info)
+{
+
+}
+
+
+void Restorer::_destroy_badge_dictionary()
+{
+	while(Badge_badge_info *info = _badge_dictionary.first())
+	{
+		_badge_dictionary.remove(info);
+		Genode::destroy(_alloc, info);
+	}
+}
+
+
+Restorer::Restorer(Genode::Allocator &alloc, Target_child &child, Target_state &state)
+: _alloc(alloc), _child(child), _state(state), _badge_dictionary() { }
+
+
+Restorer::~Restorer()
+{
+	_destroy_badge_dictionary();
+}
+
 
 void Restorer::restore()
 {
 	if(verbose_debug) Genode::log("Resto::\033[33m", __func__, "\033[0m()");
-	// Create RPC objects with checkpointed state
+	//Create RPC objects with checkpointed state
 	//_prepare_rm_sessions();
 	//_prepare_log_sessions();
 	//_prepare_timer_sessions();
 
-	// Recreate state of automatically created RPC objects
-	// CPU
-	//_prepare_threads();
-	// PD
-	//_prepare_contexts();
-	//_prepare_sources();
-	//_prepare_region_map(address_space);
-	//_prepare_region_map(stack_area);
-	//_prepare_region_map(linker_area);
+	//Recreate state of automatically created RPC objects
+	//PD
+	_restore_pd_session(_child.pd(), _state._stored_pd_session);
+	//CPU
+	//_prepare_cpu_session();
+	//  _prepare_threads();
 	//RAM
-	//_prepare_ram_dataspaces();
+	//_prepare_ram_session();
+	//  _prepare_allocated_dataspaces();
 
 
-	// Adjust capability map/capability array
+	//Adjust capability map/capability array
 	//_prepare_capability_map();
 
-	// Insert capabilities into capability space
+	//Insert capabilities into capability space
 	//_prepare_capability_space();
 
 	//Genode::List<Badge_info> exclude_infos = _create_exclude_infos(address_space, stack_area, linker_area, rm_root);
 
-	// Copy memory content
-	// Genode::List<Badge_dataspace_info> visited_infos;
+	//Copy memory content
+	//Genode::List<Badge_dataspace_info> visited_infos;
 	//_update_dataspace_content(ram, visited_infos, exclude_infos);
 	//_update_dataspace_content(address_space, visited_infos, exclude_infos);
 	//_update_dataspace_content(stack_area, visited_infos, exclude_infos);
@@ -49,13 +140,14 @@ void Restorer::restore()
 	//if(rm_root) _update_dataspace_content(rm_root, visisted_infos, exclude_infos);
 
 
-	// Delete exclude_infos
+	//Delete exclude_infos
 	//_destroy_exclude_infos(exclude_infos);
-	// Delete visited_infos
+	//Delete visited_infos
 	//while(Badge_dataspace_info *info = visited_infos.first())
 	//{
 	//	visited_infos.remove(info);
 	//	Genode::destroy(_alloc, info);
 	//}
+	_destroy_badge_dictionary();
 
 }
--- src/rtcr/restorer.h
+++ src/rtcr/restorer.h
@@ -20,16 +20,65 @@ class Rtcr::Restorer
 {
 private:
 	/**
+	 * List element which translates a badge from the checkpoint process
+	 * to a badge from the restoration process
+	 */
+	struct Badge_badge_info : Genode::List<Badge_badge_info>::Element
+	{
+		Genode::uint16_t ckpt_badge;
+		Genode::uint16_t resto_badge;
+		Badge_badge_info() : ckpt_badge(0), resto_badge(0) { }
+		Badge_badge_info(Genode::uint16_t ckpt_badge, Genode::uint16_t resto_badge)
+		: ckpt_badge(ckpt_badge), resto_badge(resto_badge) { }
+
+		Badge_badge_info *find_by_ckpt_badge(Genode::uint16_t badge)
+		{
+			if(badge == ckpt_badge)
+				return this;
+			Badge_badge_info *info = next();
+			return info ? info->find_by_ckpt_badge(badge) : 0;
+		}
+		Badge_badge_info *find_by_resto_badge(Genode::uint16_t badge)
+		{
+			if(badge == resto_badge)
+				return this;
+			Badge_badge_info *info = next();
+			return info ? info->find_by_resto_badge(badge) : 0;
+		}
+
+		void print(Genode::Output &output) const
+		{
+			using Genode::Hex;
+
+			Genode::print(output, "ckpt=", ckpt_badge, ", resto=", resto_badge);
+		}
+	};
+
+	/**
 	 * Enable log output for debugging
 	 */
 	static constexpr bool verbose_debug = restorer_verbose_debug;
+	/**
+	 * Allocator for restorer's personal datastructures. The datastructures which belong to Target_state
+	 * are created with the allocator of Target_state
+	 */
+	Genode::Allocator &_alloc;
 	Target_child &_child;
 	Target_state &_state;
+	Genode::List<Badge_badge_info> _badge_dictionary;
 
-	void _prepare_threads(Genode::List<Stored_thread_info> &state_infos, Cpu_session_component &child_obj);
+
+	void _restore_pd_session(Pd_session_component &child_obj, Stored_pd_session_info &state_info);
+	void _restore_contexts(Pd_session_component &child_obj, Genode::List<Stored_signal_context_info> &state_infos);
+	void _restore_sources(Pd_session_component &child_obj, Genode::List<Stored_signal_source_info> &state_infos);
+	void _restore_region_map(Region_map_component &child_obj, Stored_region_map_info &state_info);
+	void _restore_threads(Cpu_session_component &child_obj, Genode::List<Stored_thread_info> &state_infos);
+
+	void _destroy_badge_dictionary();
 
 public:
-	Restorer(Target_child &child, Target_state &state);
+	Restorer(Genode::Allocator &alloc, Target_child &child, Target_state &state);
+	~Restorer();
 
 	void restore();
 };
