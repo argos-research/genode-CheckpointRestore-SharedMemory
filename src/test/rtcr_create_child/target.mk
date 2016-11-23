1838ea9ba0bffcfba14c45cda9d134722de5069d
DenisHuberTUM <huber.denis@mytum.de>
2016-11-23 14:03:18 +0100

    Adjusting translation units

--- src/test/rtcr_create_child/target.mk
+++ src/test/rtcr_create_child/target.mk
@@ -1,30 +1,32 @@
 TARGET = rtcr_child_creator
 
 SRC_CC += main.cc \
-          cpu_session_component.cc \
-          cpu_thread_component.cc \
-          pd_session_component.cc \
-          ram_session_component.cc \
-          rom_session_component.cc \
-          region_map_component.cc \
-          log_session.cc \
+          pd_session.cc \
+          cpu_session.cc \
+          ram_session.cc \
+          rom_session.cc \
           rm_session.cc \
+          log_session.cc \
           timer_session.cc \
+          cpu_thread_component.cc \
+          region_map_component.cc \
           target_child.cc \
+          target_state.cc \
           checkpointer.cc \
           restorer.cc
 
 LIBS   += base
 
-vpath cpu_session_component.cc $(REP_DIR)/src/rtcr/intercept
-vpath cpu_thread_component.cc  $(REP_DIR)/src/rtcr/intercept
-vpath pd_session_component.cc  $(REP_DIR)/src/rtcr/intercept
-vpath ram_session_component.cc $(REP_DIR)/src/rtcr/intercept
-vpath rom_session_component.cc $(REP_DIR)/src/rtcr/intercept
-vpath region_map_component.cc  $(REP_DIR)/src/rtcr/intercept
-vpath log_session.cc           $(REP_DIR)/src/rtcr/intercept
+vpath pd_session.cc            $(REP_DIR)/src/rtcr/intercept
+vpath cpu_session.cc           $(REP_DIR)/src/rtcr/intercept
+vpath ram_session.cc           $(REP_DIR)/src/rtcr/intercept
+vpath rom_session.cc           $(REP_DIR)/src/rtcr/intercept
 vpath rm_session.cc            $(REP_DIR)/src/rtcr/intercept
+vpath log_session.cc           $(REP_DIR)/src/rtcr/intercept
 vpath timer_session.cc         $(REP_DIR)/src/rtcr/intercept
+vpath cpu_thread_component.cc  $(REP_DIR)/src/rtcr/intercept
+vpath region_map_component.cc  $(REP_DIR)/src/rtcr/intercept
 vpath target_child.cc          $(REP_DIR)/src/rtcr
+vpath target_state.cc          $(REP_DIR)/src/rtcr
 vpath checkpointer.cc          $(REP_DIR)/src/rtcr
 vpath restorer.cc              $(REP_DIR)/src/rtcr
