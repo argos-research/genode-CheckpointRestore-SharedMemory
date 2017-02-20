0e97fa5a4a2e76b8486f9ebde9c854be369d1edf
DenisHuberTUM <huber.denis@mytum.de>
2017-02-20 10:56:33 +0100

    Adding classes to associate a badge or capability to a specific kcap

---
+++ src/rtcr/util/kcap_cap_info.h
@@ -0,0 +1,54 @@
+/*
+ * \brief  List element to store the designated kcap address for a given capability
+ * \author Denis Huber
+ * \date   2016-02-20
+ */
+
+#ifndef _RTCR_KCAP_CAP_INFO_H_
+#define _RTCR_KCAP_CAP_INFO_H_
+
+/* Genode includes */
+#include <util/list.h>
+
+/* Rtcr includes */
+
+namespace Rtcr {
+	struct Kcap_cap_info;
+}
+
+/**
+ * List element to store the designated kcap address for a given capability
+ */
+struct Rtcr::Kcap_cap_info : Genode::List<Kcap_cap_info>::Element
+{
+	Genode::addr_t            kcap;
+	Genode::Native_capability cap;
+
+	Kcap_cap_info(Genode::addr_t kcap, Genode::Native_capability cap)
+	: kcap(kcap), cap(cap) { }
+
+	Kcap_cap_info *find_by_kcap(Genode::addr_t kcap)
+	{
+		if(kcap == this->kcap)
+			return this;
+		Kcap_cap_info *info = next();
+		return info ? info->find_by_kcap(kcap) : 0;
+	}
+
+	Kcap_cap_info *find_by_badge(Genode::uint16_t badge)
+	{
+		if(badge == this->cap.local_name())
+			return this;
+		Kcap_cap_info *info = next();
+		return info ? info->find_by_badge(badge) : 0;
+	}
+
+	void print(Genode::Output &output) const
+	{
+		using Genode::Hex;
+
+		Genode::print(output, "kcap=", Hex(kcap), ", cap=", cap);
+	}
+};
+
+#endif /* _RTCR_KCAP_CAP_INFO_H_ */
