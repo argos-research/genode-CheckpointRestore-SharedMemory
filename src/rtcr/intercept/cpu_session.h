de86f0c1405295ca661031d5d55a06dca7cfd07b
Alexander <reisner.alex93@gmail.com>
2017-10-08 19:34:47 +0200

    Add killed time alias soft exit

--- src/rtcr/intercept/cpu_session.h
+++ src/rtcr/intercept/cpu_session.h
@@ -115,15 +115,13 @@ public:
 	 * KIA4SM methods
 	 */
 
-	Genode::Thread_capability create_fp_edf_thread(Genode::Pd_session_capability pd_cap,
-			Name const &name, Genode::Affinity::Location affinity, Weight weight,
-			Genode::addr_t utcb, unsigned priority, unsigned deadline) override;
 	int set_sched_type(unsigned core, unsigned sched_type) override;
 	int get_sched_type(unsigned core) override;
 	void set(Genode::Ram_session_capability ram_cap) override;
 	void deploy_queue(Genode::Dataspace_capability ds) override;
 	void rq(Genode::Dataspace_capability ds) override;
 	void dead(Genode::Dataspace_capability ds) override;
+	void killed() override;
 };
 
 
